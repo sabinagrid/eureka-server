a9788e2fc1bc9aab6d3bf89fdcfac560c5a66638
sabinagrid <sradu@griddynamics.com>
2025-03-07 17:10:45 +0200

    removed .idea from repo

--- src/main/java/com/taskmanagement/EurekaServerApplication.java
+++ src/main/java/com/taskmanagement/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.taskmanagement.eurekaserver;
+package com.taskmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
